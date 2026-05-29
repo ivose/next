5859cd5dae0cef8d8a1b8352e155ca94db1f5705
ivose <ivoseeba@gmail.com>
2026-05-29 20:38:17 +0300

    task1 - blog list

---
+++ app/blogs/page.tsx
@@ -0,0 +1,22 @@
+import { getBlogs } from "../services/blogs"
+
+const Blogs = () => {
+  const blogs = getBlogs()
+
+  return (
+    <div>
+      <h2>blogs</h2>
+      <ul>
+        {blogs.map(blog => (
+          <li key={blog.id}>
+            <a href={blog.url} target="_blank" rel="noopener noreferrer">
+              {blog.title}
+            </a>{" "} by {blog.author}, likes {blog.likes}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default Blogs
---
+++ app/services/blogs.ts
@@ -0,0 +1,17 @@
+const blogs = [
+  // id, title, author, url, and likes.
+  { id: 1, title: "Otsikko 1", author: "A1", url: "https://cs.ut.ee", likes: 4 },
+  { id: 2, title: "Otsikko 2", author: "A2", url: "https://quretec.com", likes: 1 },
+  { id: 3, title: "Otsikko 3", author: "A3", url: "https://www.helsinki.fi/fi", likes: 2 },
+  { id: 3, title: "Otsikko 3", author: "A3", url: "https://delta.ut.ee", likes: 2 },
+]
+
+let nextId = Math.max(...blogs.map(blog => blog.id))
+
+export const getBlogs = () => {
+  return blogs
+}
+
+export const addBlog = (title: string, author: string, url: string, likes: number) => {
+  blogs.push({ id: nextId++, title, author, url, likes })
+}
