555dadbc9cc567a6c28f8888bfe969b9fe76847e
ivose <ivoseeba@gmail.com>
2026-05-30 07:13:58 +0300

    task6, search

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -3,12 +3,31 @@ import { getBlogs } from "../services/blogs"
 
 export const dynamic = "force-dynamic"
 
-const Blogs = () => {
-  const blogs = getBlogs()
+const Blogs = async ({searchParams}: { searchParams: Promise<{ filter?: string }>}) => {
+  const { filter } = await searchParams
+
+  const blogs = [...getBlogs()]
+    .filter(blog =>
+      filter
+        ? blog.title.toLowerCase().includes(filter.toLowerCase())
+        : true
+    )
+    .sort((a, b) => b.likes - a.likes)
 
   return (
     <div>
       <h2>blogs</h2>
+
+      <form action="/blogs">
+        <input
+          type="text"
+          name="filter"
+          defaultValue={filter}
+          placeholder="search by title"
+        />
+        <button type="submit">search</button>
+      </form>
+
       <ul>
         {blogs.map(blog => (
           <li key={blog.id}>
--- app/services/blogs.ts
+++ app/services/blogs.ts
@@ -1,18 +1,44 @@
-const blogs = [
+type Blog = {
+  id: number
+  title: string
+  author: string
+  url: string
+  likes: number
+}
+
+const initialBlogs: Blog[] = [
   { id: 1, title: "Otsikko 1", author: "A1", url: "https://cs.ut.ee", likes: 4 },
   { id: 2, title: "Otsikko 2", author: "A2", url: "https://quretec.com", likes: 1 },
   { id: 3, title: "Otsikko 3", author: "A3", url: "https://www.helsinki.fi/fi", likes: 2 },
   { id: 4, title: "Otsikko 4", author: "A4", url: "https://delta.ut.ee", likes: 2 },
 ]
 
-let nextId = Math.max(...blogs.map(blog => blog.id)) + 1
+const globalForBlogs = globalThis as unknown as {
+  blogs: Blog[] | undefined
+  nextBlogId: number | undefined
+}
+
+if (!globalForBlogs.blogs) {
+  globalForBlogs.blogs = initialBlogs
+  globalForBlogs.nextBlogId = Math.max(...initialBlogs.map(blog => blog.id)) + 1
+}
+
+const blogs = globalForBlogs.blogs
 
 export const getBlogs = () => {
   return blogs
 }
 
 export const addBlog = (title: string, author: string, url: string) => {
-  blogs.push({ id: nextId++, title, author, url, likes: 0 })
+  blogs.push({
+    id: globalForBlogs.nextBlogId!,
+    title,
+    author,
+    url,
+    likes: 0,
+  })
+
+  globalForBlogs.nextBlogId! += 1
 }
 
 export const getBlogById = (id: number) => {
