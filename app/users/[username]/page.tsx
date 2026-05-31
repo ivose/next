cb4c444e60e6d11d2329115b1cbd67e4630dd75a
ivose <ivoseeba@gmail.com>
2026-05-31 18:49:21 +0300

    task12, Registration, dynamic

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link"
 import { notFound } from "next/navigation"
 import { getUserWithBlogs } from "../../services/users"
 
+export const dynamic = "force-dynamic"
+
 const UserPage = async ({
   params,
 }: {
