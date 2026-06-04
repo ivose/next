5ff718339422ce70b4001aa6b82cd2770736ed5c
ivose <ivoseeba@gmail.com>
2026-06-04 20:20:10 +0300

    task24, The final check

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from "drizzle-kit"
 import * as dotenv from "dotenv"
-// Load .env.test in test environment, otherwise .env.local
+
 const envFile = process.env.NODE_ENV === "test" ? ".env.test" : ".env.local"
+
 dotenv.config({ path: envFile })
+dotenv.config({ path: ".env.local" })
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is missing")
+}
 
 export default defineConfig({
   schema: "./db/schema.ts",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
 })
